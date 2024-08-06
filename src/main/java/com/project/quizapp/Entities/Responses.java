9dd7f177c06dcfc32890428cbab67fff448af971
thepallavi02 <pallavi.singh9810@gmail.com>
2024-08-06 18:18:13 +0530

    Initial commit

---
+++ src/main/java/com/project/quizapp/Entities/Responses.java
@@ -0,0 +1,11 @@
+package com.project.quizapp.Entities;
+
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@RequiredArgsConstructor
+public class Responses {
+    private Integer id;
+    private String response;
+}
