1b9d83bd66acaf575d99885f73cba6dbb90db53f
pallavisingh-aidash <pallavi.singh@aidash.com>
2024-08-06 17:16:16 +0530

    Initial Commit

---
+++ src/main/java/com/project/quizapp/Entities/Quiz.java
@@ -0,0 +1,17 @@
+package com.project.quizapp.Entities;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@Entity
+public class Quiz {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String title;
+    @ManyToMany
+    private List<Questions> questionsList;
+}
